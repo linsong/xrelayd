19151fb45b4628462c9e15f42356c5e60346180a
Vincent Wang <vincent@linsong.org>
2016-02-18 17:23:11 +0800

    change function/defines text's name

--- xrelayd.c
+++ xrelayd.c
@@ -55,13 +55,13 @@
 #define SYSLOG_NAMES
 #include <syslog.h>
 
-/* xassl includes */
-#include <xyssl/config.h>
-#include <xyssl/havege.h>
-#include <xyssl/certs.h>
-#include <xyssl/x509.h>
-#include <xyssl/ssl.h>
-#include <xyssl/net.h>
+/* mbedtls includes */
+#include <mbedtls/config.h>
+#include <mbedtls/havege.h>
+#include <mbedtls/certs.h>
+#include <mbedtls/x509.h>
+#include <mbedtls/ssl.h>
+#include <mbedtls/net.h>
 
 // FIXME. implement some sort of DDOS prevention
 #define MAXCONNCOUNT 16
@@ -91,14 +91,14 @@ void dolog(int prio, const char *fmt, ...);
  */
 int xrly_ciphers[] =
 {
-#if defined(XYSSL_AES_C)
+#if defined(MBEDTLS_AES_C)
     SSL_RSA_AES_128_SHA,
     SSL_RSA_AES_256_SHA,
 #endif
-#if defined(XYSSL_DES_C)
+#if defined(MBEDTLS_DES_C)
     SSL_RSA_DES_168_SHA,
 #endif
-#if defined(XYSSL_ARC4_C)
+#if defined(MBEDTLS_ARC4_C)
     SSL_RSA_RC4_128_SHA,
     SSL_RSA_RC4_128_MD5,
 #endif
@@ -119,7 +119,7 @@ int loglevel = LOG_NOTICE;
 
 void sigchld_handler(int s)
 {
-        while(waitpid(-1, NULL, WNOHANG) > 0);
+    while(waitpid(-1, NULL, WNOHANG) > 0);
 }
 
 void kill_handler(int s)
@@ -240,16 +240,16 @@ int handle_sockres(char* op, char* conn,int res)
     if(res>0) return 0;
     
     switch( res ) {
-        case XYSSL_ERR_NET_TRY_AGAIN:
+        case MBEDTLS_ERR_NET_TRY_AGAIN:
             DLOG( "%s operation on %s connection would block",op,conn);
         case 0:
             return 0;
             
-        case XYSSL_ERR_SSL_PEER_CLOSE_NOTIFY:
+        case MBEDTLS_ERR_SSL_PEER_CLOSE_NOTIFY:
             ILOG( "%s connection closed by peer during %s operation",conn,op);
             break;
 
-        case XYSSL_ERR_NET_CONN_RESET:
+        case MBEDTLS_ERR_NET_CONN_RESET:
             ILOG( "%s connection was reset by peer during %s operation",conn,op);
             break;
             
@@ -264,10 +264,10 @@ int handle_sockres(char* op, char* conn,int res)
  * These session callbacks use a simple chained list
  * to store and retrieve the session information.
  */
-ssl_session *s_list_1st = NULL;
-ssl_session *cur, *prv;
+mbedtls_ssl_session *s_list_1st = NULL;
+mbedtls_ssl_session *cur, *prv;
 
-static int my_get_session( ssl_context *ssl )
+static int my_get_session( mbedtls_ssl_context *ssl )
 {
     time_t t = time( NULL );
 
@@ -299,7 +299,7 @@ static int my_get_session( ssl_context *ssl )
     return( 1 );
 }
 
-static int my_set_session( ssl_context *ssl )
+static int my_set_session( mbedtls_ssl_context *ssl )
 {
     time_t t = time( NULL );
 
@@ -320,7 +320,7 @@ static int my_set_session( ssl_context *ssl )
 
     if( cur == NULL )
     {
-        cur = (ssl_session *) malloc( sizeof( ssl_session ) );
+        cur = (mbedtls_ssl_session *) malloc( sizeof( mbedtls_ssl_session ) );
         if( cur == NULL )
             return( 1 );
 
@@ -329,7 +329,7 @@ static int my_set_session( ssl_context *ssl )
         else  prv->next  = cur;
     }
 
-    memcpy( cur, ssl->session, sizeof( ssl_session ) );
+    memcpy( cur, ssl->session, sizeof( mbedtls_ssl_session ) );
 
     return( 0 );
 }
@@ -340,13 +340,13 @@ inline int proxy_send_all( int (*f_send)(void *, unsigned char *, int),
     int     ret=0;
     while(len) {
         if ((ret = f_send(ctx,buf,len)) <= 0) {
-            if( ret == XYSSL_ERR_NET_TRY_AGAIN ) continue;
+            if( ret == MBEDTLS_ERR_NET_TRY_AGAIN ) continue;
             break;
         }
         len-=ret;
         buf+=ret;
     }
-    *eof |= (ret == XYSSL_ERR_NET_CONN_RESET);
+    *eof |= (ret == MBEDTLS_ERR_NET_CONN_RESET);
     return ret;
 }
 
@@ -355,7 +355,7 @@ inline int proxy_recv_available( int (*f_recv)(void *, unsigned char *, int),
 {
     int     ret=0;
     ret = f_recv(ctx,buf,len);
-    *eof |= (ret == XYSSL_ERR_NET_CONN_RESET);
+    *eof |= (ret == MBEDTLS_ERR_NET_CONN_RESET);
     return ret;
 }
 
@@ -370,7 +370,7 @@ void proxy_connection(
      */
     int     server_fd;
     ILOG("Attempting to connect to %s:%d", srv_host, srv_port);
-    if( ( ret = net_connect( &server_fd, srv_host, srv_port ) ) != 0 ) {
+    if( ( ret = mbedtls_net_connect( &server_fd, srv_host, srv_port ) ) != 0 ) {
         ELOG("Failed to connect to %s:%d: %08x, %s", srv_host, srv_port, ret, strerror(errno));
         return;
     }
@@ -379,41 +379,41 @@ void proxy_connection(
     /*
      *  Setup ssl
      */
-    ssl_context     ssl;
-    if( ( ret = ssl_init( &ssl ) ) != 0 ) {
+    mbedtls_ssl_context     ssl;
+    if( ( ret = mbedtls_ssl_init( &ssl ) ) != 0 ) {
         ELOG("Failed to initialize ssl: %08x", ret);
         return;
     }
     
     /* setup endpoint */
-    ssl_set_endpoint( &ssl, sslserver );
+    mbedtls_ssl_set_endpoint( &ssl, sslserver );
     
     /* FIXME: verify hook for client connections. */
-    ssl_set_authmode( &ssl, SSL_VERIFY_NONE );
+    mbedtls_ssl_set_authmode( &ssl, SSL_VERIFY_NONE );
 
     /* random number generation */
     havege_state hs;
     havege_init( &hs );
-    ssl_set_rng( &ssl, havege_rand, &hs );
+    mbedtls_ssl_set_rng( &ssl, havege_rand, &hs );
     
     /* io */
     int     *ssl_fd = sslserver ? &client_fd : &server_fd;
     int     *plain_fd = sslserver ? &server_fd : &client_fd;
-    ssl_set_bio( &ssl, net_recv, ssl_fd,
-                       net_send, ssl_fd );
+    mbedtls_ssl_set_bio( &ssl, mbedtls_net_recv, ssl_fd,
+                       mbedtls_net_send, ssl_fd );
     
     /* ciphers */
-    ssl_set_ciphers( &ssl, xrly_ciphers );
+    mbedtls_ssl_set_ciphers( &ssl, xrly_ciphers );
     
     if(cert && key) {
-        ssl_set_ca_chain( &ssl, cert->next, NULL );
-        ssl_set_own_cert( &ssl, cert, key );
+        mbedtls_ssl_set_ca_chain( &ssl, cert->next, NULL );
+        mbedtls_ssl_set_own_cert( &ssl, cert, key );
     }
     
     /* session caching */
-    ssl_session ssn;
-    ssl_set_scb( &ssl, my_get_session, my_set_session );
-    ssl_set_session( &ssl, 1, 0, &ssn );
+    mbedtls_ssl_session ssn;
+    mbedtls_ssl_set_scb( &ssl, my_get_session, my_set_session );
+    mbedtls_ssl_set_session( &ssl, 1, 0, &ssn );
     
     ILOG("Initialized SSL for %s mode",sslserver ? "server" : "client");
     
@@ -431,8 +431,8 @@ void proxy_connection(
      */
     if(sslserver) {
         ILOG("Performing ssl handshake");
-        while( ( ret = ssl_handshake( &ssl ) ) != 0 ) {
-            if( ret != XYSSL_ERR_NET_TRY_AGAIN ) {
+        while( ( ret = mbedtls_ssl_handshake( &ssl ) ) != 0 ) {
+            if( ret != MBEDTLS_ERR_NET_TRY_AGAIN ) {
                 ELOG("SSL handshake failed: %08x", ret);
                 return;
             }
@@ -456,8 +456,8 @@ void proxy_connection(
     
     unsigned char buf[2048];
     
-    net_set_nonblock(*plain_fd);
-    net_set_nonblock(*ssl_fd);
+    mbedtls_net_set_nonblock(*plain_fd);
+    mbedtls_net_set_nonblock(*ssl_fd);
     
     int done=0,rret,wret;
     
@@ -485,11 +485,11 @@ void proxy_connection(
             DLOG("ssl fd is set");
             do {
                 DLOG("trying to read from ssl fd %d",*ssl_fd);
-                if((rret = proxy_recv_available(ssl_read, &ssl, buf, sizeof(buf), &done)) < 0) break;
+                if((rret = proxy_recv_available(mbedtls_ssl_read, &ssl, buf, sizeof(buf), &done)) < 0) break;
                 DLOG("read: %d bytes",rret);
                 
                 DLOG("trying to write on plain fd %d",*plain_fd);
-                if((wret = proxy_send_all(net_send, (void*)plain_fd, buf, rret, &done)) < 0) break;
+                if((wret = proxy_send_all(mbedtls_net_send, (void*)plain_fd, buf, rret, &done)) < 0) break;
                 DLOG("write: plain complete");
             } while(rret > 0 && rret == wret);
             
@@ -504,30 +504,30 @@ void proxy_connection(
             DLOG("plain fd is set");
             do {
                 DLOG("trying to read from plain fd %d",*plain_fd);
-                if((rret = proxy_recv_available(net_recv, plain_fd, buf, sizeof(buf), &done)) < 0) break;
+                if((rret = proxy_recv_available(mbedtls_net_recv, plain_fd, buf, sizeof(buf), &done)) < 0) break;
                 DLOG("read: %d bytes",rret);
                 
                 DLOG("trying to write on ssl fd %d",*ssl_fd);
-                if((wret = proxy_send_all(ssl_write, (void*)&ssl, buf, rret, &done)) < 0) break;
+                if((wret = proxy_send_all(mbedtls_ssl_write, (void*)&ssl, buf, rret, &done)) < 0) break;
                 DLOG("write: ssl complete");
             } while(rret > 0 && rret == wret);
             
             if(handle_sockres("read","plain",rret)) break;
             if(handle_sockres("write","ssl",wret)) break;
-            ssl_flush_output(&ssl);
+            mbedtls_ssl_flush_output(&ssl);
         }
     }
     
     /*
      *  Cleanup
      */
-    ssl_free( &ssl );
+    mbedtls_ssl_free( &ssl );
     memset( &ssl, 0, sizeof( ssl ) );
     
-    net_close(client_fd);
+    mbedtls_net_close(client_fd);
     ILOG("Closed %s connection from %d.%d.%d.%d",client_fd==*ssl_fd ? "ssl" : "plain",
         client_ip[0],client_ip[1],client_ip[2],client_ip[3]);
-    net_close(server_fd);
+    mbedtls_net_close(server_fd);
     ILOG("Closed %s connection to %s:%d",server_fd==*ssl_fd ? "ssl" : "plain",srv_host,srv_port);
     
     NLOG("Closed connection between %s client %d.%d.%d.%d and %s server %s:%d",
@@ -700,10 +700,10 @@ int main(int argc, char** argv)
     }
 
 /* install handlers */
-    signal(SIGCHLD,sigchld_handler); /* ignore child */
-    signal(SIGQUIT,kill_handler); /* catch hangup signal */
-    signal(SIGHUP,kill_handler); /* catch hangup signal */
-    signal(SIGTERM,kill_handler); /* catch kill signal */
+    signal(SIGCHLD, sigchld_handler); /* ignore child */
+    signal(SIGQUIT, kill_handler); /* catch quit signal */
+    signal(SIGHUP,  kill_handler); /* catch hangup signal */
+    signal(SIGTERM, kill_handler); /* catch kill signal */
     
     if(!nosysl) {
         openlog("xrelayd", LOG_PID, LOG_DAEMON);
@@ -805,7 +805,7 @@ int main(int argc, char** argv)
     
     // open listening socket
     int             srv_fd, client_fd;
-    ret = net_bind( &srv_fd, srv_host, srv_port );
+    ret = mbedtls_net_bind( &srv_fd, srv_host, srv_port );
     if(ret) {
         ELOG("Failed to open server port %d: %08x, %s",srv_port,ret,strerror(errno));
         goto fail;
@@ -826,7 +826,7 @@ int main(int argc, char** argv)
             continue;
         }
         
-        ret = net_accept( srv_fd, &client_fd, client_ip );
+        ret = mbedtls_net_accept( srv_fd, &client_fd, client_ip );
         if(ret) {
             ELOG("Failed to accept client on server port %d: %08x, %s",srv_port,ret,strerror(errno));
             goto fail;
@@ -860,7 +860,7 @@ succeed:
 fail:
     if(srv_port) {
         NLOG("Closing server port %d",srv_port);
-        net_close(srv_fd);
+        mbedtls_net_close(srv_fd);
     }
 
     x509_free( &cert );
